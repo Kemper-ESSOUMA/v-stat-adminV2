cc727f7573dd34b9f9a1bf5887a0b7b610f6767f
KPR-dev <kemperessouma@gmail.com>
2025-01-26 12:08:12 +0100

    Update IndexvGraphe.vue

--- src/views/v2/graphe/IndexvGraphe.vue
+++ src/views/v2/graphe/IndexvGraphe.vue
@@ -1,5 +1,294 @@
 <template>
-  <div>
+  <div class="header-contentys pt-3 d-flex align-items-center">
+    <div class="mr-5">
+      <h4 class="text-app font-weight-bold">Analyse des données</h4>
+    </div>
+  </div>
+  <hr>
 
+  <!-- Conteneur pour les graphiques avec disposition en colonne -->
+  <div class="card d-flex flex-column align-items-center justify-content-between">
+
+    <!-- Premier graphique en haut -->
+    <div class="chart-container" style="width: 90%; margin-bottom: 10px;">
+      <ProgressBar mode="indeterminate" style="height: 6px" v-if="loading === true"></ProgressBar>
+      <canvas id="prov" width="400" height="200"></canvas>
+    </div>
+
+    <!-- Deuxième graphique en bas -->
+    <div class="chart-container" style="width: 90%;">
+      <canvas id="secondChart" width="500" height="300"></canvas>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.chart-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+
+<style scoped>
+.chart-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+
+<script>
+import Chart from 'chart.js/auto';
+import { useAppStore } from "@/store/app";
+
+export default {
+  data: () => ({
+    ws: null,
+    datas: [],
+    get_all_province: [],
+    province: [],
+    items: [
+      {
+        title: "Analyse de données",
+        disabled: true,
+      },
+      {
+        title: "Accueil",
+        disabled: false,
+        href: "/",
+      },
+    ],
+  }),
+
+  mounted() {
+    this.connectWebSocket();
+    this.get_all_provinces();
+    this.get_vote_centre_vote();
+    this.zoneCode = this.currentUser().zone_code;
+  },
+
+
+  methods: {
+
+    connectWebSocket() {
+      // Définir l'URL du WebSocket (à adapter selon votre serveur)
+      this.ws = new WebSocket(this.$wsUrl);
+
+      // Gestion des événements WebSocket
+      this.ws.onopen = () => {
+        console.log("WebSocket connecté !");
+        console.log("Données initiales :", this.datas); // Affichage des données initiales
+      };
+
+      this.ws.onmessage = (event) => {
+        console.log("Événement WebSocket : Message reçu");
+
+        try {
+          const message = event.data; // Si c'est un JSON, il faut le parser
+          console.log("Message reçu via WebSocket :", message);
+
+          // Si le message contient des données mises à jour
+          if (message && message.updatedData) {
+            // Mettre à jour les données
+            this.datas = [...this.datas, ...message.updatedData]; // Ajoutez les nouvelles données
+
+            // Redessiner les graphiques avec les nouvelles données
+            this.renderSecondChart();
+            this.renderChartProvince(); 
+          }
+
+          if (message && message.updatedProvinces) {
+            // Mettre à jour les données des provinces
+            this.province = message.updatedProvinces;
+
+            // Redessiner le graphique des provinces
+            this.renderChartProvince(); // Redessiner le graphique des provinces
+          }
+        } catch (error) {
+          console.error(
+            "Erreur lors de la réception des données WebSocket :",
+            error
+          );
+        }
+      };
+
+      this.ws.onerror = (error) => {
+        console.error("Erreur WebSocket :", error);
+      };
+
+      this.ws.onclose = () => {
+        console.log("WebSocket fermé ! Tentative de reconnexion...");
+        setTimeout(() => {
+          this.connectWebSocket(); // Reconnexion après 3 secondes
+        }, 3000);
+      };
+    },
+
+    currentUser() {
+      const appStore = useAppStore(); // Assurez-vous d'importer correctement useAppStore
+      return appStore.currentUser; // Récupérer les informations utilisateur
+    },
+
+    get_all_provinces() {
+      this.$axios.get("/resultat/get_vote_by_province").then((response) => {
+        this.province = response.data;
+        this.renderChartProvince();
+      });
+    },
+
+    get_vote_centre_vote() {
+      this.$axios.get(`/voting_centre/by_zone`).then((response) => {
+        this.datas = response.data;
+        console.log('datas centre by zone=', this.datas);
+        this.renderSecondChart();
+      });
+    },
+
+    renderChartProvince() {
+
+      console.log("datas=", this.province)
+      var labels = Object.keys(this.province[0]).slice(1)
+      // var newLabels = ['Candidat 1', 'Candidat 2', 'Candidat 3'];
+      var values = this.province.map(item => Object.values(item).slice(1))
+
+      // var newValues = ['ESTUAIRE', 'HAUT-OGOOUE', 'MOYEN OGOOUE', 'NGOUNIE', 'NYANGA', 'OGOOUE IVINDO', 'OGOOUE-LOLO', 'OGOOUE-MARITIME', 'WOLEU-NTEM'];
+      var colors = ['red', 'blue', 'green', 'purple', 'orange', 'yellow']
+      console.log('labels=', labels)
+      console.log('values=', values)
+
+      var datasets = labels.map((label, index) => ({
+        label: label,
+        data: values.map(item => item[index]),
+        backgroundColor: colors[index]
+      }))
+
+      const ctx = document.getElementById('prov').getContext('2d');
+
+
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: this.province.map(item => item.province),
+          datasets: datasets,
+        },
+        options: {
+          responsive: true,
+          legend: {
+            position: 'top',
+          },
+          scales: {
+            x: {
+              title: {
+                display: true,
+                text: 'Provinces'
+              },
+            },
+            y: {
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Valeurs'
+              }
+            }
+          },
+          plugins: {
+            legend: {
+              display: true,
+            },
+            title: {
+              display: false,
+            },
+          },
+          barPercentage: 2.9,
+        }
+
+      });
+    },
+
+    renderSecondChart() {
+      // Extraire dynamiquement les noms des candidats
+      const candidates = Object.keys(this.datas[0]).filter(key => key.startsWith('candidate_')); // Filtrer les clés commençant par 'candidate_'
+
+      // Préparer les labels pour les centres de vote
+      const labels = this.datas.map(item => item.libelle); // Utilisation du libellé pour les centres de vote
+
+      // Préparer un tableau pour chaque candidat avec les votes par centre
+      const datasets = candidates.map((candidate, index) => {
+        return {
+          label: `Candidat ${index + 1}`,  // Nom dynamique du candidat
+          data: this.datas.map(item => item[candidate] || 0), // Récupérer les votes pour chaque candidat, avec 0 par défaut
+          backgroundColor: `rgba(${(index * 40 + 50) % 255}, ${(index * 60 + 80) % 255}, ${(index * 80 + 100) % 255}, 0.6)`, // Couleurs dynamiques pour chaque candidat
+          borderColor: `rgba(${(index * 40 + 50) % 255}, ${(index * 60 + 80) % 255}, ${(index * 80 + 100) % 255}, 1)`,
+          borderWidth: 1,
+        };
+      });
+
+      console.log('labels vote=', labels);
+      console.log('datasets vote=', datasets);
+
+      // Récupérer le contexte du canvas
+      const ctx = document.getElementById('secondChart').getContext('2d');
+
+      // Créer le graphique avec Chart.js
+      new Chart(ctx, {
+        type: 'bar', // Type de graphique (bar)
+        data: {
+          labels: labels, // Centres de vote comme labels
+          datasets: datasets, // Datasets pour chaque candidat
+        },
+        options: {
+          responsive: true, // Rend le graphique réactif
+          plugins: {
+            legend: {
+              display: true, // Affiche la légende
+              position: 'top', // Position de la légende
+            },
+          },
+          scales: {
+            x: {
+              title: {
+                display: true, // Affiche le titre de l'axe X
+                text: 'Centres de vote', // Titre de l'axe X
+              },
+              barPercentage: 0.5, // Ajuste la largeur des barres
+            },
+            y: {
+              beginAtZero: true, // Commence l'axe Y à 0
+              title: {
+                display: true, // Affiche le titre de l'axe Y
+                text: 'Votes', // Titre de l'axe Y
+              },
+            },
+          },
+        },
+      });
+    },
+
+
+  },
+};
+</script>
+
+<style>
+.chart-container {
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
